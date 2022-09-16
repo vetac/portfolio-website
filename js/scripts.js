b02c3fe5f063735b6a4dad939642a6bf9a01b5c1
vetac <vetac@users.noreply.github.com>
2022-09-15 21:13:55 -0400

    Update scripts.js
    Added JavaScript for hamburger menu toggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,10 @@
 console.log("Hello! Welcome to my website! Hope you'll find what you want here");
 
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+        x.className += ' responsive';
+      } else {
+        x.className = 'navtoggle';
+      }
+}
