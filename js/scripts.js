c6b46da57324aec0678c1c7ab80557715c15bb79
vetac <vetac@users.noreply.github.com>
2022-09-18 08:48:47 -0400

    Updated code for hamburger menu
    Updated for all HTML and js files - Menu automatically closes upon interacting with 'contact'

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
-
+/* Hamgueger Menu */
 function menuToggle() {
     var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {
-        x.className += ' responsive';
-      } else {
-        x.className = 'navtoggle';
-      }
-}
+      var isMobileBreakpoint = window.innerWidth <= 641
+  
+    if (x.className === 'navtoggle' && isMobileBreakpoint) {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
